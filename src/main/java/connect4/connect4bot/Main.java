9c6ca85f74e24ed32f5c3f8b882ed83305a054a2
Joe <josephfielding03@gmail.com>
2023-11-01 13:43:13 -0400

    added javafx

--- src/main/java/connect4/connect4bot/Main.java
+++ src/main/java/connect4/connect4bot/Main.java
@@ -1,3 +1,5 @@
+package connect4.connect4bot;
+
 import java.util.Arrays;
 
 public class Main {
@@ -20,17 +22,17 @@ public class Main {
         System.out.println(checkWin(encodePos(pos), 5));
         long start = System.currentTimeMillis();
         long posNum = encodePos(pos);
-        for(long i = 0; i < 40000000; i++){
+        for (long i = 0; i < 40000000; i++) {
             boolean win = checkWin(i, (int) i % 42);
         }
         System.out.println(System.currentTimeMillis() - start);
     }
 
-    static long encodePos(String board){
+    static long encodePos(String board) {
         String[] cols = new String[7];
         Arrays.fill(cols, "");
-        for(String row : board.split("\n")){
-            for(int i = 0; i < 7; i++){
+        for (String row : board.split("\n")) {
+            for (int i = 0; i < 7; i++) {
                 cols[i] += row.charAt(i);
             }
         }
@@ -38,7 +40,7 @@ public class Main {
                 + (Long.parseLong(cols[0], 2) << 36) + (Long.parseLong(cols[1], 2) << 30) + (Long.parseLong(cols[2], 2) << 24) + (Long.parseLong(cols[3], 2) << 18) + (Long.parseLong(cols[4], 2) << 12) + (Long.parseLong(cols[5], 2) << 6) + (Long.parseLong(cols[6], 2));
     }
 
-    public static boolean checkWin(long state, int lastMove){
+    public static boolean checkWin(long state, int lastMove) {
         int row = lastMove % 6, col = lastMove / 6;
         return connectionCount(state, lastMove, row, col, 0, 1, 6) + connectionCount(state, lastMove, row, col, 0, -1, -6) >= 3 ||
                 connectionCount(state, lastMove, row, col, -1, 0, -1) >= 3 ||
@@ -46,12 +48,12 @@ public class Main {
                 connectionCount(state, lastMove, row, col, 1, -1, -5) + connectionCount(state, lastMove, row, col, -1, 1, 5) >= 3;
     }
 
-    static int connectionCount(long grid, int lastMove, int row, int col, int dr, int dc, int dp){
+    static int connectionCount(long grid, int lastMove, int row, int col, int dr, int dc, int dp) {
         int count = 0;
         int r = row + dr;
         int c = col + dc;
         int pos = lastMove + dp;
-        while(r > -1 && r < 6 && c > -1 && c < 7 && (grid >> pos & 1) == 1){
+        while (r > -1 && r < 6 && c > -1 && c < 7 && (grid >> pos & 1) == 1) {
             count++;
             r += dr;
             c += dc;
@@ -60,16 +62,16 @@ public class Main {
         return count;
     }
 
-    static long flipTurn(long grid){
+    static long flipTurn(long grid) {
         int[] heights = new int[7];
-        for(int i = 0; i < 7; i++){
+        for (int i = 0; i < 7; i++) {
             heights[i] = (int) (grid >> (42 + 3 * i) & 7);
         }
         System.out.println(Arrays.toString(heights));
         long flippedGrid = 0L;
-        for(int i = 0; i < 7; i++){
+        for (int i = 0; i < 7; i++) {
             int height = heights[i];
-            if(height == 0) continue;
+            if (height == 0) continue;
             int col = (int) (grid >> (i * 6) & 63) ^ ((1 << (height)) - 1);
             System.out.print((grid >> (i * 6) & 63));
             System.out.println(" " + (grid >> (i * 6) & 63 ^ 63));
