9e047b4ca723e122f53921f01377586e8f47f080
Joe <josephfielding03@gmail.com>
2023-11-01 13:45:15 -0400

    added javafx

---
+++ src/main/java/connect4/connect4bot/HelloController.java
@@ -0,0 +1,14 @@
+package connect4.connect4bot;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
