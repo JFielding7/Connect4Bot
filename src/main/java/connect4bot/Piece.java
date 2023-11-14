49e3ba2f62aa3f944b614473d7146cb5b7b93b80
nmbar <nmbarton1@loyola.edu>
2023-11-13 23:48:41 -0500

    encoding and decoding

--- src/main/java/connect4bot/Piece.java
+++ src/main/java/connect4bot/Piece.java
@@ -1,4 +1,26 @@
 package connect4bot;
 
 public class Piece {
+    String color;
+    int p;
+
+    public Piece() {p=0;}
+
+    public void set(int p) {
+        this.p = p;
+        color = p==1 ? "red" : "yellow";
+    }
+
+    public boolean isEmpty() {
+        return p==0;
+    }
+
+    public byte getBit() {
+        return (byte) (p==1 ? 1 : 0);
+    }
+
+    public String toString() {
+        if(p==0) return " ";
+        return p==1 ? "+" : "o";
+    }
 }
